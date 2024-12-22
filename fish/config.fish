d6d9c1f7f4b79ff4a5678b87ded8fa0ce990a857
Acestus <architect@acestus.com>
2024-12-22 00:03:57 -0600

    Daily Commit

---
+++ fish/config.fish
@@ -0,0 +1,38 @@
+if status is-interactive
+
+end
+
+set -U fish_greeting
+
+#Aliases
+alias ls="lsd -F"
+alias la="lsd -AF"
+alias ll="lsd -lAF"
+alias lg="lsd -F --group-dirs=first"
+alias oh-my-posh='/home/acestus/.local/bin/oh-my-posh'
+#oh-my-posh init fish | source
+
+# Functions
+function gd
+    git add .
+    git commit -m "Daily Commit"
+    git push
+end
+
+function dn
+    set dir "/home/acestus/git/workspace24/md/planner/"
+    cd "$dir" || return
+    set filename (date +"%m-%d").md
+    touch "$filename"
+
+    # Add the header to the file with the date
+    if test ! -s "$filename"
+        echo (date) > "$filename"
+    end
+
+    nvim "$filename"
+end
+
+
+# Generated for envman. Do not edit.
+test -s ~/.config/envman/load.fish; and source ~/.config/envman/load.fish
